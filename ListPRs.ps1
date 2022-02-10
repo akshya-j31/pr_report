fde835371fe8be3afacedb59a8d8d8ad4c47082e
Akshya Jain <akshya.j31@gmail.com>
2022-02-10 09:23:45 +0530

    Pull the Report of all Open and closed PRs in Last 7 days
    Added PS script to Pull Report of all Open and closed PRs in Last 7 days

---
+++ ListPRs.ps1
@@ -0,0 +1,7 @@
+﻿$Output = Invoke-RestMethod https://api.github.com/repos/pulumi/pulumi/pulls?state=all
+$LastSevenDaysPRs = $Output | Where-Object {[DateTime]($_.created_at) -gt  (Get-Date).AddDays(-7)} `
+                    | select created_at,state,html_url,title
+
+Write-Output $LastSevenDaysPRs
+
+Send-MailMessage -From "abc@org.com" -To "xyz@org.com" -Subject "Last 7 Days PR report" -Body "$LastSevenDaysPRs" -SmtpServer "smtp.org.com" -Credential org.com\mailuser -UseSsl
